774e4031e1357ad33071c734c0ac5e4cf97476bc
KokaKerze <mr.kertsman@gmail.com>
2021-06-17 21:01:57 +0300

    mk lint2

--- src/account/views.py
+++ src/account/views.py
@@ -23,7 +23,7 @@ class ShowProfilePageView(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         """Return context data for Profile."""
-        avatar = Avatar.objects.all()
+        # avatar = Avatar.objects.all()
         context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context['page_user'] = page_user
